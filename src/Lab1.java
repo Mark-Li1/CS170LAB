109faa9b50c0603929cc65fddcfa50f33089523d
Mark Li <xXstockfishXx@gmail.com>
2025-09-06 12:44:35 -0400

    Testing Commit

---
+++ src/Lab1.java
@@ -0,0 +1,12 @@
+
+// Name : Mark Li
+// mli585@emory.edu / 2768161
+// THIS CODE WAS MY OWN WORK , IT WAS WRITTEN WITHOUT
+// CONSULTING ANY SOURCES OUTSIDE OF THOSE APPROVED BY THE INSTRUCTOR .
+// Mark Li
+
+public class Lab1 {
+    public static void main(String[] args){
+        System.out.println("Hello , World ! My name is Mark Li!\nHow are you doing today ?\nIts a nice day to complete Lab 1!");
+    }
+}
