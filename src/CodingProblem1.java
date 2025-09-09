4bd27f994d6f47ea64cd53d98288c602932750f5
Mark Li <xXstockfishXx@gmail.com>
2025-09-09 12:44:48 -0400

    Testing Commit

---
+++ src/CodingProblem1.java
@@ -0,0 +1,19 @@
+// Name : Mark Li
+// mli585@emory.edu / 2768161
+// THIS CODE WAS MY OWN WORK , IT WAS WRITTEN WITHOUT
+// CONSULTING ANY SOURCES OUTSIDE OF THOSE APPROVED BY THE INSTRUCTOR .
+// Mark Li
+
+public class CodingProblem1 {
+    public static void main(String[] args){
+        int wholenum;
+        float floatnum;
+        double doublenum = 100.001;
+        char alpha = 'Z';
+        wholenum = 1234;
+        floatnum = 99.99f;
+
+        System.out.println("floatnum + doublenum = " + (floatnum + doublenum));
+        System.out.println("wholenum + alpha = " + (wholenum + alpha));
+    }
+}
